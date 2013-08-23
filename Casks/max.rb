a819d454da071d78e492a9c3e13fee247dbf1e4d
Allan Taschini <allan.taschini@gmail.com>
2013-08-23 15:02:10 +0200

    Added Max SHA1.

--- Casks/max.rb
+++ Casks/max.rb
@@ -2,6 +2,6 @@ class Max < Cask
   url 'http://files.sbooth.org/Max-0.9.1.tar.bz2'
   homepage 'http://sbooth.org/Max/'
   version '0.9.1'
-  sha1 ''
+  sha1 '6943a30c102bf815c89002671f0f0fd64c1d2805'
   link :none
 end
