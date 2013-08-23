88e4f1f984f0c5bf3930ebf6463db3d548e82e4c
Allan Taschini <allan.taschini@gmail.com>
2013-08-23 17:44:01 +0200

    Added ComicTagger sha1

--- Casks/comictagger.rb
+++ Casks/comictagger.rb
@@ -2,6 +2,6 @@ class Comictagger < Cask
   url 'https://comictagger.googlecode.com/files/ComicTagger-1.1.9-beta.dmg'
   homepage 'https://code.google.com/p/comictagger/'
   version '1.1.9-beta'
-  sha1 ''
+  sha1 'a98c730c2f41bec4c7b9ecfb57c13167f11bc6df'
   link :none
 end
