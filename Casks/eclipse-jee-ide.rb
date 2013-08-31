06aef3bc2b8596e063f18c533f2b75607f05ad9a
Allan Taschini <allan.taschini@gmail.com>
2013-08-31 12:06:01 +0200

    Added Eclipse JEE initial cask

---
+++ Casks/eclipse-jee-ide.rb
@@ -0,0 +1,7 @@
+class EclipseJeeIde < Cask
+  url 'http://download.eclipse.org/technology/epp/downloads/release/kepler/R/eclipse-jee-kepler-R-macosx-cocoa-x86_64.tar.gz'
+  homepage 'http://eclipse.org'
+  version '4.3'
+  sha1 ''
+  link 'Eclipse.app'
+end
