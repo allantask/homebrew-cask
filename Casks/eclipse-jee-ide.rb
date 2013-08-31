ca0568f34bbc0ce96e6e9090b0333832003e3b35
Allan Taschini <allan.taschini@gmail.com>
2013-08-31 15:20:53 +0200

    Added Eclipse EE checksum.

--- Casks/eclipse-jee-ide.rb
+++ Casks/eclipse-jee-ide.rb
@@ -2,6 +2,6 @@ class EclipseJeeIde < Cask
   url 'http://download.eclipse.org/technology/epp/downloads/release/kepler/R/eclipse-jee-kepler-R-macosx-cocoa-x86_64.tar.gz'
   homepage 'http://eclipse.org'
   version '4.3'
-  sha1 ''
+  sha1 '8b5fb40da4f77c0d412eeab75379ab4c86913ee7'
   link 'Eclipse.app'
 end
