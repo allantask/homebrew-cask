9a60afebbf4a1c98303f128404763b0ef56e5699
Allan Taschini <allan.taschini@gmail.com>
2013-12-28 12:15:00 +0100

    Updated to 4.3.1

--- Casks/eclipse-jee-ide.rb
+++ Casks/eclipse-jee-ide.rb
@@ -1,7 +1,7 @@
 class EclipseJeeIde < Cask
-  url 'http://download.eclipse.org/technology/epp/downloads/release/kepler/R/eclipse-jee-kepler-R-macosx-cocoa-x86_64.tar.gz'
+  url 'http://eclipse.tsl.gr/technology/epp/downloads/release/kepler/SR1/eclipse-jee-kepler-SR1-macosx-cocoa-x86_64.tar.gz'
   homepage 'http://eclipse.org'
-  version '4.3'
-  sha1 '8b5fb40da4f77c0d412eeab75379ab4c86913ee7'
+  version '4.3.1'
+  sha1 ''
   link 'Eclipse.app'
 end
